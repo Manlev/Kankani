764ca2c2117a10fbbc3e9e4d15b1b793d0e6ee38
Lucy Lee <lucy-lee>
2018-10-16 16:19:35 +0900

    first commit

---
+++ ManlevDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManlevDiary
+//
+//  Created by 이명경 on 2018. 10. 16..
+//  Copyright © 2018년 Manlev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
